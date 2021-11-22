78af52fc14049c1df64d7aa3446662015b33ec0d
The Crypto <32523275+feripratama@users.noreply.github.com>
2021-11-22 15:14:01 +0700

    Update lccopy.js

--- lccopy.js
+++ lccopy.js
@@ -1,2 +1,4 @@
 console.log("%cAuthor: Team TIK & E-GOV DISKOMFO - Kab Sarolangun", "color: red; font-size:15px; font-weight: bold;");
 console.log("%cWebsite: https://sarolangunkab.go.id/", "color: red; font-size:15px; font-weight: bold;");
+
+alert('ijin dulu woi')
