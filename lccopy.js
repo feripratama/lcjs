a2f90266af8d21afc9004088df23032927198ecd
The Crypto <32523275+feripratama@users.noreply.github.com>
2021-11-22 15:12:50 +0700

    Update lccopy.js

--- lccopy.js
+++ lccopy.js
@@ -1,2 +1,2 @@
-console.log("%cAuthor: Feri Pratama", "color: red; font-size:15px; font-weight: bold;");
-console.log("%cWebsite: https://feripratama.github.io/", "color: red; font-size:15px; font-weight: bold;");
+console.log("%cAuthor: Team TIK & E-GOV DISKOMFO - Kab Sarolangun", "color: red; font-size:15px; font-weight: bold;");
+console.log("%cWebsite: https://sarolangunkab.go.id/", "color: red; font-size:15px; font-weight: bold;");
